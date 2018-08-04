396ded8562f0e1e8e4ea7806d6dfd34118477cdc
Zhao Jiangkun <jiangkun.zhao@ele.me>
2018-08-04 23:52:15 +0800

    Update pages

--- .circleci/scripts/deploy-ghpages.sh
+++ .circleci/scripts/deploy-ghpages.sh
@@ -4,6 +4,7 @@ cd public
 git config --global user.email "jiangkun.zhao90@gmail.com"
 git config --global user.name "Zhao Jiangkun"
 git init
+git remote add origin ${CIRCLE_REPOSITORY_URL}
 git add .
 git commit -m "Deploy to Github pages"
 git checkout -b gh-pages
