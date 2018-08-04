a31485cbab6e9c356ab208e0b1e86feaf6bb56b4
Zhao Jiangkun <jiangkun.zhao@ele.me>
2018-08-04 23:43:33 +0800

    Update scripts

--- .circleci/scripts/deploy-ghpages.sh
+++ .circleci/scripts/deploy-ghpages.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 cd public
+git config --global user.email "jiangkun.zhao90@gmail.com"
+git config --global user.name "Zhao Jiangkun"
 git init
 git add .
 git commit -m "Deploy to Github pages"
